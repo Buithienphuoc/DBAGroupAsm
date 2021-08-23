fb247f40fb7c5ab280241209a6dea985ee0f7b15
Viet-1999 <s3759306@rmit.edu.vn>
2021-08-23 20:48:08 +0700

    Modified the create-tables.sql file.

--- create-tables.sql
+++ create-tables.sql
@@ -81,3 +81,40 @@ create table transaction_history
 
 alter table transaction_history
     add primary key (id);
+    
+drop table if exists branch;
+drop table if exists auction_product;    
+drop table if exists customer_account;
+drop table if exists transaction_history;
+
+insert into branch (branch_code, branch_name, address, hotline_number)
+values 
+(3753, 'Laptop/Computer', '45-4, Nagata-cho 2-chome, Chiyoda-ku, Tokyo', '+8170-162-6220'),
+(9488, 'TV', '3200 Clifford Street, Concord, California', '510-697-9932'),
+(7555, 'Phone' '813-6, Hannam-dong, Yongsan-gu, Seoul', '+82-1-621-8419'),
+(7999, 'Camera', 'Via Belviglieri 107, Roma', '0372 0788675');
+
+insert into auction_product (id, minimum_price, closing_time, product_status, current_maximum_bid_price, seller_id)
+values 
+(1, 60.37, '16:00', 'Active', 85.13, 5),
+(2, 96.20, '13:00', 'Active', 133.57, 6),
+(3, 85.13, '12:00', 'Active', 52.86, 7),
+(4, 69.49, '21:00', 'Active', 67.36, 8);
+
+insert into customer_account (id, phone, first_name, last_name, email, account_password, address, city, country, budget, balance, profile_picture, branch_id)
+values 
+(1, '+8139-346-0840', 'Nobuko', 'Yagi', 'Nobuko@mail.com', '461908', '470-1148, Oichiharacho, Izumiotsu-shi, Osaka', 'Osaka', 'Japan', 1),
+(2, '904-480-5838', 'Charlie', 'Fernandez', 'Charlie@mail.com', '061306', '4472 Arrowood Drive, Orlando, Florida', 'Florida', 'USA', 2),
+(3, '+82-2-285-5596', 'Ji', 'Pak', 'Ji@mail.com', '389755', '297-10, Yeomchang-dong, Gangseo-gu, Seoul', 'Seoul', 'Korea', 3),
+(4, '0394 4316429', 'Emanuele', 'Folliero', 'Emanuele@mail.com', '902416', 'Via dei Fiorentini 65, Napoli', 'Napoli', 'Italy', 4),
+(5, '+8179-124-7504', 'Yasuhiro', 'Hada', 'Yasuhiro@mail.com', '184094', '243-1255, Nishishinjuku Shinjuku Sumitomobiru(28-kai), Shinjuku-ku, Tokyo', 'Tokyo', 'Japan', 1),
+(6, '320-650-2631', 'Baldwin', 'Robson', 'Baldwin@mail.com', '247326', '4755 Brighton Circle Road, Saint Cloud, Minnesota', 'Minnesota', 'USA', 2),
+(7, '+82-4-292-8600', 'Mun-Hee', 'An', 'Mun-Hee@mail.com', '455395', '225-1, Hyeondaeapateu, Ganseog 4(sa)-dong, Namdong-gu, Incheon', 'Incheon', 'Korea', 3),
+(8, '0314 6594092', 'Gregario' 'Ferri', 'Gregario@gmail.com', '794545', 'Via Antonio Cecchi 1037, Venezia', 'Venezia', 'Italy', 4);
+
+insert into transaction_history (id, product_id, buyer_id, bid_price, recorded_at)
+values 
+(1, 1, 1, 32.30, 2021-11-11),
+(2, 2, 2, 41.66, 2021-12-21),
+(3, 3, 3, 26.04, 2022-05-21),
+(4, 4, 4, 20.63, 2022-01-31);
