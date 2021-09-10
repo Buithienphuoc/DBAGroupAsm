1717b15afd2695cdd029035c6019abdceacec624
Buithienphuoc <tphuoc1998@gmail.com>
2021-09-10 15:10:36 +0700

    "Increase and decrease money of buyer and seller"

---
+++ admin/read_product.php
@@ -0,0 +1,73 @@
+<?php
+require "../connection.php";
+
+session_start();
+?>
+
+<?php
+
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
+
+// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+$stmt = $pdo->prepare('SELECT * FROM auction_product WHERE CURRENT_TIMESTAMP >= closing_time ORDER BY product_status = \'Active\' DESC ');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM auction_product')->fetchColumn();
+
+?>
+
+<?=template_header('Read')?>
+
+<div class="content read">
+    <h2>Read Contacts</h2>
+    <table>
+        <thead>
+        <tr>
+            <td>#</td>
+            <td>Product Name</td>
+            <td>Start price</td>
+            <td>Closed time</td>
+            <td>Product Status</td>
+            <td>End Price</td>
+            <td>Seller ID</td>
+            <td>Buyer ID</td>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($products as $product): ?>
+            <tr>
+                <td><?=$product['id']?></td>
+                <td><?=$product['product_name']?></td>
+                <td><?=$product['minimum_price']?></td>
+                <td><?=$product['closing_time']?></td>
+                <td><?=$product['product_status']?></td>
+                <td><?=$product['current_maximum_bid_price']?></td>
+                <td><?=$product['seller_id']?></td>
+                <td><?=$product['buyer_id']?></td>
+                <td class="actions">
+                    <a href="update_product.php?id=<?=$product['id']?>" class="edit">Update status<i></i></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+    <div class="pagination">
+        <?php if ($page > 1): ?>
+            <a href="read_product.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+        <?php endif; ?>
+        <?php if ($page*$records_per_page < $num_contacts): ?>
+            <a href="read_product.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
---
+++ admin/update_product.php
@@ -0,0 +1,75 @@
+<?php
+include '../connection.php';
+
+$msg = '';
+// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
+
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        // This part is similar to the create.php, but instead we update a record and not insert
+        $id = $_POST['id'] ?? NULL;
+        $product_name = $_POST['product_name'] ?? '';
+        $minimum_price = $_POST['minimum_price'] ?? '';
+        $closing_time= $_POST['closing_time'] ?? '';
+        $product_status = $_POST['product_status'] ?? '';
+        $current_maximum_bid_price = (int) $_POST['current_maximum_bid_price'] ?? '';
+        $seller_id = $_POST['seller_id'] ?? '';
+        $buyer_id = $_POST['buyer_id'] ?? '';
+
+        // Update the record
+        $stmt = $pdo->prepare('UPDATE auction_product SET product_status = ? WHERE id = ?');
+        $update_seller = $pdo->prepare('UPDATE customer_account SET balance = balance + ? WHERE id = ?');
+        $update_buyer = $pdo->prepare('UPDATE customer_account SET balance = balance - ? WHERE id = ?');
+        $stmt->execute([$product_status, $_GET['id']]);
+        var_dump($current_maximum_bid_price);
+        $update_seller->execute([$current_maximum_bid_price, $seller_id]);
+        $update_buyer->execute([$current_maximum_bid_price, $buyer_id]);
+        $msg = 'Updated Successfully!';
+    }
+    // Get the contact from the contacts table
+    $stmt = $pdo->prepare('SELECT * FROM auction_product WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$product) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+
+} else {
+    exit('No ID specified!');
+}
+
+?>
+
+<?=template_header('Read')?>
+
+<div class="content update">
+    <h2> Create transaction #<?=$product['id']?></h2>
+    <form action="update_product.php?id=<?=$product['id']?>" method="post">
+        <label for="id">ID</label>
+        <input type="text" name="id" value="<?=$product['id']?>" id="id" readonly="readonly">
+        <label for="minimum_price">Minimum price</label>
+        <input type="number" name="minimum_price" value="<?=$product['minimum_price']?>" id="name" readonly="readonly">
+        <label for="current_maximum_bid_price">Current maximum price</label>
+        <input type="number" name="current_maximum_bid_price" value="<?=$product['current_maximum_bid_price']?>" id="name" readonly="readonly" >
+        <label for="product_name">Product name</label>
+        <input type="text" name="product_name" value="<?=$product['product_name']?>" id="id" readonly="readonly">
+        <label for="closing_time">Closed time</label>
+        <input type="text" name="closing_time" value="<?=$product['closing_time']?>" id="email" readonly="readonly">
+        <label for="seller_id">Seller id</label>
+        <input type="text" name="seller_id" value="<?=$product['seller_id']?>" id="seller_id" readonly="readonly">
+        <label for="buyer_id">Buyer id</label>
+        <input type="text" name="buyer_id" value="<?=$product['buyer_id']?>" id="buyer_id" readonly="readonly">
+        <label for="product_status">Status</label><br>
+        <select name="product_status" id="product_status">
+            <option name="inactive" value="Inactive">Inactive</option>
+        </select>
+        <input type="submit" value="Update" onclick="location.href='read_product.php';">
+    </form>
+
+    <?php if ($msg): ?>
+        <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
+
