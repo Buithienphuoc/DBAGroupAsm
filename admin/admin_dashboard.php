a1fc9e8ec7f65e68e3ce9c1be956222d36a68fe3
Buithienphuoc <tphuoc1998@gmail.com>
2021-09-11 19:20:42 +0700

    "Fix admin dashboard"

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -8,5 +8,9 @@ include '../connection.php';
 <?=template_header('Home')?>
     <a href="read_customer.php">Update Customer's balance</a>
     </br>
-    <a href="view_all_transactions.php">View all transactions</a>
+    <a href="view_all_transactions.php">View all transactions and Calculate total bid price</a>
+    </br>
+    <a href="read_product.php">Update product status and creating transactions</a>
+    </br>
+    <a href="read_transaction.php">View and Undo transaction</a>
 <?=template_footer()?>
