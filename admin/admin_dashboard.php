04b0c0c4bfb90a56d0552a1efd3ef78bfb494f81
Buithienphuoc <tphuoc1998@gmail.com>
2021-09-11 19:42:23 +0700

    "Fix"

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -4,13 +4,17 @@ include '../connection.php';
 
 // Home Page template below.
 ?>
-
+    <div style="float:right;">
+        <ul>
+            <li><a class="btn btn-danger" href="logout_admin.php" style="color: pink">Log out</a></li>
+        </ul>
+    </div>
 <?=template_header('Home')?>
     <a class="btn btn-info" href="read_customer.php">Update Customer's balance</a>
     </br>
     <a class="btn btn-info" href="view_all_transactions.php">View all transactions and Calculate total bid price</a>
     </br>
-    <a class="btn btn-danger" href="read_product.php">Making product INACTIVE to create transactions</a>
+    <a class="btn btn-danger" href="read_product.php">Make product INACTIVE to create transactions</a>
     </br>
     <a class="btn btn-danger" href="read_transaction.php">View and Undo transaction</a>
 <?=template_footer()?>
